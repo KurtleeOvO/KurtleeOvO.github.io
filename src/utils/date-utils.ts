620ad87d5755e2e195461c3add919fae69667de6
kurt <leek61741@gmail.com>
2026-05-27 11:31:33 +0800

    修改Github Pages1.0

--- src/utils/date-utils.ts
+++ src/utils/date-utils.ts
@@ -3,12 +3,16 @@ export function formatDateToYYYYMMDD(date: Date): string {
 }
 
 export function getISOWeekYear(date: Date): { year: number; week: number } {
-	const d = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+	const d = new Date(
+		Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()),
+	);
 	const day = d.getUTCDay() || 7;
 	d.setUTCDate(d.getUTCDate() + 4 - day);
 	const year = d.getUTCFullYear();
 	const yearStart = new Date(Date.UTC(year, 0, 1));
-	const week = Math.ceil(((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7);
+	const week = Math.ceil(
+		((d.getTime() - yearStart.getTime()) / 86400000 + 1) / 7,
+	);
 	return { year, week };
 }
 
